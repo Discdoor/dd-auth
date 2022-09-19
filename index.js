273728f2facff8d4e4d0fd88ec9d424483033b61
ctrlz <r@sys36.net>
2022-09-19 23:26:03 +0200

    add account registration endpoint

--- index.js
+++ index.js
@@ -10,15 +10,43 @@ const { MongoClient } = require('mongodb');
 const UserManager = require('./lib/mgmt/usermgr');
 const dbClient = new MongoClient(cfg.db.url);
 
-app.use(express.json());
+// Set a reasonable limit for the JSON body parser
+app.use(express.json({limit: '10kb'}));
+
+// Vars
+/** @type {UserManager} */
+let userMgr;
 
 // ++++++++++++++++++ END POINTS ++++++++++++++++++ //
 
 /**
  * Registration endpoint - registers a user.
  */
-app.post(`/auth/register`, (req, res)=>{
-    
+app.post(`/auth/register`, async(req, res)=>{
+    res.contentType('json');
+
+    try {
+        // Check types
+        if((typeof req.body === 'undefined') || (typeof req.body.dob !== 'number'))
+            return res.sendStatus(400);
+
+        const result = await userMgr.createUser(req.body.email, req.body.username, req.body.password, new Date(req.body.dob));
+
+        res.end(JSON.stringify({
+            username: result.username,
+            discrim: result.discrim,
+            email: result.email,
+            verifStatus: result.verifStatus,
+            avatarUrl: result.avatarUrl 
+        }));
+    } catch(e) {
+        console.error(e);
+        res.status(400);
+        res.end(JSON.stringify({
+            code: (e.code != null) ? e.code : -1,
+            message: (e.message != null) ? e.message : e
+        }));
+    }
 });
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++ //
@@ -32,15 +60,14 @@ async function main() {
 
     try {
         await dbClient.connect();
+        const db = dbClient.db('discdoor');
+        userMgr = new UserManager(db);
         console.log("DB connection success!");
     } catch(e) {
         console.log("Error: Database connection failure, see exception below:");
         console.error(e);
         process.exit(1);
     }
-
-    const umgr = await new UserManager(dbClient.db('discdoor'));
-    console.log(await umgr.createUser("Abc@defg.com", "Abcdefg", "abcdefg", new Date()));
 
     // Step 2: Start HTTP server
     app.listen(cfg.http.port, async () => {
